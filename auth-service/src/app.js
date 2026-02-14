763741e312fcb864693d12c46872d69bf1427809
lukia <lukian10s@gmail.com>
2026-02-14 14:54:05 -0300

    Trigger CI/CD for all services

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.get('/health', (req, res) => res.json({ status: 'Auth Service OK' }));
 app.post('/login', AuthController.login);
 
-app.listen(3002, '0.0.0.0', () => console.log('Auth Service rodando na porta 3002'));
+app.listen(3002, '0.0.0.0', () => console.log('Auth Service Rodando na porta 3002'));
